c428d563d2b18ea92a27ae0b01a5d3f74afc1ab9
Eduardo Marino Riscado <eduardomarino@gmail.com>
2019-11-26 20:56:51 -0300

    feat: add mongoose setup

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./middleware/logger');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 // Route files
 const user = require('./routes/user');
 const patient = require('./routes/patient');
 const appointment = require('./routes/appointment');
-
-// Load env vars
-dotenv.config({ path: './config/config.env' });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -23,6 +27,13 @@ app.use('/api/v1/user', user);
 app.use('/api/v1/patient', patient);
 app.use('/api/v1/appointment', appointment);
 
-app.listen(PORT,
+const server = app.listen(PORT,
   console.log(`>>> Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`)
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`>>> Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
