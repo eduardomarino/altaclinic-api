c97c422146b8ceb3b18fd0c486a78709a2eac4da
Eduardo Marino Riscado <eduardomarino@gmail.com>
2020-02-19 20:31:23 -0300

    feat: add get logged user

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,6 +53,23 @@ exports.login = async (req, res, next) => {
   }
 }
 
+// @desc      Get logged user
+// @route     GET /api/v1/auth/me
+// @access    Private
+exports.getMe = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    return res.status(200).json({
+      success: true,
+      data: user
+    });
+
+  } catch(err) {
+    return next(err);
+  }
+}
+
 // Get token, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   const token = user.getSignedToken();
