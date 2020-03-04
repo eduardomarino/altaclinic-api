41c19d76f81736667412f04c079c4c50be59d57d
Eduardo Marino Riscado <eduardomarino@gmail.com>
2020-03-04 20:37:26 -0300

    feat: improve appointments controller

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,38 +1,97 @@
+const Appointment = require('../models/Appointment');
+const ErrorResponse = require('../utils/errorResponse');
+
 // @desc      Get all appointments
 // @route     GET /api/v1/appointments
-exports.getAllAppointments = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all appointments' });
+exports.getAllAppointments = async (req, res, next) => {
+  try {
+    return res.status(200).json(res.resultsHandler);
+
+  } catch(err) {
+    return next(err);
+  }
 }
 
 // @desc      Get single appointment
 // @route     GET /api/v1/appointments/:appointmentId
-exports.getAppointment = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Get appointment ${req.params.appointmentId}`
-  });
+exports.getAppointment = async (req, res, next) => {
+  try {
+    const appointment = await Appointment.findById(req.params.appointmentId);
+
+    if (!appointment) {
+      return next(
+        new ErrorResponse(`Appointment not found with id: ${req.params.appointmentId}`, 404)
+      );
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: appointment
+    });
+
+  } catch(err) {
+    return next(err);
+  }
 }
 
 // @desc      Create new appointment
 // @route     POST /api/v1/appointments
-exports.createAppointment = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new appointment' });
+exports.createAppointment = async (req, res, next) => {
+  try {
+    const appointment = await Appointment.create(req.body);
+    return res.status(201).json({
+      success: true,
+      data: appointment
+    });
+
+  } catch(err) {
+    return next(err);
+  }
 }
 
 // @desc      Update appointment
 // @route     PUT /api/v1/appointments/:appointmentId
-exports.updateAppointment = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Update appointment ${req.params.appointmentId}`
-  });
+exports.updateAppointment = async (req, res, next) => {
+  try {
+    const appointment = await Appointment.findByIdAndUpdate(req.params.appointmentId, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!appointment) {
+      return next(
+        new ErrorResponse(`Appointment not found with id: ${req.params.appointmentId}`, 404)
+      );
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: appointment
+    });
+
+  } catch(err) {
+    return next(err);
+  }
 }
 
 // @desc      Delete appointment
 // @route     DELETE /api/v1/appointments/:appointmentId
-exports.deleteAppointment = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Delete appointment ${req.params.appointmentId}`
-  });
+exports.deleteAppointment = async (req, res, next) => {
+  try {
+    const appointment = await Appointment.findByIdAndDelete(req.params.appointmentId);
+
+    if (!appointment) {
+      return next(
+        new ErrorResponse(`Appointment not found with id: ${req.params.appointmentId}`, 404)
+      );
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: {}
+    });
+
+  } catch(err) {
+    return next(err);
+  }
 }
