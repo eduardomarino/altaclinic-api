0421e9dad0f99cc9e21210397f811daed663fb44
Eduardo Marino Riscado <eduardomarino@gmail.com>
2020-03-04 20:53:38 -0300

    feat: improve appointments routes

--- routes/appointments.js
+++ routes/appointments.js
@@ -8,9 +8,17 @@ const {
   deleteAppointment
 } = require('../controllers/appointments');
 
+const Appointment = require('../models/Appointment');
+
+const resultsHandler = require('../middleware/results');
+const { protect, authorize } = require('../middleware/auth');
+
+router.use(protect);
+router.use(authorize('admin', 'physician', 'user'));
+
 router
   .route('/')
-  .get(getAllAppointments)
+  .get(resultsHandler(Appointment), getAllAppointments)
   .post(createAppointment)
 ;
 
