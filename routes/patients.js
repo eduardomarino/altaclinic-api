5e60c563d6bb20c605516b9f636523574497c557
Eduardo Marino Riscado <eduardomarino@gmail.com>
2020-03-11 21:35:40 -0300

    chore: adjust patients routes

--- routes/patients.js
+++ routes/patients.js
@@ -5,32 +5,7 @@ const {
   getPatient,
   createPatient,
   updatePatient,
-  deletePatient,
-  getAllPrescriptions,
-  getPrescription,
-  createPrescription,
-  updatePrescription,
-  deletePrescription,
-  getAllEvaluations,
-  getEvaluation,
-  createEvaluation,
-  updateEvaluation,
-  deleteEvaluation,
-  getAllExam,
-  getExam,
-  createExam,
-  updateExam,
-  deleteExam,
-  getAllMedicalRecord,
-  getMedicalRecord,
-  createMedicalRecord,
-  updateMedicalRecord,
-  deleteMedicalRecord,
-  getAllAppointmentHistory,
-  getAppointmentHistory,
-  createAppointmentHistory,
-  updateAppointmentHistory,
-  deleteAppointmentHistory
+  deletePatient
 } = require('../controllers/patients');
 
 const Patient = require('../models/Patient');
@@ -52,71 +27,6 @@ router
   .get(getPatient)
   .put(updatePatient)
   .delete(authorize('admin'), deletePatient)
-;
-
-router
-  .route('/:patientId/prescription')
-  .get(getAllPrescriptions)
-  .post(createPrescription)
-;
-
-router
-  .route('/:patientId/prescription/:prescriptionId')
-  .get(getPrescription)
-  .put(updatePrescription)
-  .delete(deletePrescription)
-;
-
-router
-  .route('/:patientId/evaluation')
-  .get(getAllEvaluations)
-  .post(createEvaluation)
-;
-
-router
-  .route('/:patientId/evaluation/:evaluationId')
-  .get(getEvaluation)
-  .put(updateEvaluation)
-  .delete(deleteEvaluation)
-;
-
-router
-  .route('/:patientId/exam')
-  .get(getAllExam)
-  .post(createExam)
-;
-
-router
-  .route('/:patientId/exam/:examId')
-  .get(getExam)
-  .put(updateExam)
-  .delete(deleteExam)
-;
-
-router
-  .route('/:patientId/medical-record')
-  .get(getAllMedicalRecord)
-  .post(createMedicalRecord)
-;
-
-router
-  .route('/:patientId/medical-record/:medicalRecordId')
-  .get(getMedicalRecord)
-  .put(updateMedicalRecord)
-  .delete(deleteMedicalRecord)
-;
-
-router
-  .route('/:patientId/appointment-history')
-  .get(getAllAppointmentHistory)
-  .post(createAppointmentHistory)
-;
-
-router
-  .route('/:patientId/appointment-history/:appointmentHistoryId')
-  .get(getAppointmentHistory)
-  .put(updateAppointmentHistory)
-  .delete(deleteAppointmentHistory)
 ;
 
 module.exports = router;
