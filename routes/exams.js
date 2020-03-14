327f7434c61968dad0e1c1816f17f3bb79d8cb6c
Eduardo Marino Riscado <eduardomarino@gmail.com>
2020-03-14 09:47:05 -0300

    feat: add exams routes

---
+++ routes/exams.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getAllExams,
+  getExam,
+  createExam,
+  updateExam,
+  deleteExam
+} = require('../controllers/exams');
+
+const Exam = require('../models/Exam');
+
+const resultsHandler = require('../middleware/results');
+const { protect, authorize } = require('../middleware/auth');
+
+router.use(protect);
+router.use(authorize('admin', 'physician'));
+
+router
+  .route('/')
+  .get(resultsHandler(Exam), getAllExams)
+  .post(createExam)
+;
+
+router
+  .route('/:patientId')
+  .get(getExam)
+  .put(updateExam)
+  .delete(deleteExam)
+;
+
+module.exports = router;
