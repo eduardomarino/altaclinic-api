f51ca3bdb42e7ab3cb707c9fd8f1a4bcc8e0eb66
Eduardo Marino Riscado <eduardomarino@gmail.com>
2020-03-16 15:16:55 -0300

    feat: add prescription model

---
+++ models/Prescription.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const PrescriptionSchema = new mongoose.Schema({
+  prescription: {
+    type: String,
+    required: [true, 'A prescription is required.'],
+    trim: true,
+    maxlength: [500, 'Prescription can not be more than 500 characters.']
+  },
+  patient: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Patient',
+    required: [true, 'A patient is required.']
+  },
+  physician: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'A physician is required.']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Prescription', PrescriptionSchema);
