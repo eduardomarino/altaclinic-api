47630e1eb82b8e8882a5ed9bdbe38a67659da58d
Eduardo Marino Riscado <eduardomarino@gmail.com>
2020-03-16 15:36:21 -0300

    feat: add exam model

---
+++ models/Exam.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const PrescriptionSchema = new mongoose.Schema({
+  exam: {
+    type: String,
+    required: true,
+    enum: [
+      'Type 1',
+      'Type 2',
+      'Type 3',
+      'Type 4'
+    ]
+  },
+  detail: {
+    type: String,
+    required: [true, 'A detail is required.'],
+    trim: true,
+    maxlength: [500, 'Detail can not be more than 500 characters.']
+  },
+  patient: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Patient',
+    required: [true, 'A patient is required.']
+  },
+  physician: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'A physician is required.']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Prescription', PrescriptionSchema);
