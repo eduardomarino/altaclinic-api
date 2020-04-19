360f87772b8b2ce673f6104b6eb88fa346356473
Eduardo Marino Riscado <eduardomarino@gmail.com>
2020-04-19 12:41:44 -0300

    feat: enable cors in dev mode

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -30,9 +31,11 @@ app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
-// Dev logging middleware
+// Dev logging middleware and enables cors
 if (process.env.NODE_ENV === 'development') {
   app.use(logger);
+  app.use(cors());
+  app.options('*', cors());
 }
 
 // Mount routers
