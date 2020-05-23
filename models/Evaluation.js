1d040fa57fb8c597df9792c49ccc425439ac1f2f
Eduardo Marino Riscado <eduardomarino@gmail.com>
2020-05-23 13:07:40 -0300

    feat: add date time in evaluation model

--- models/Evaluation.js
+++ models/Evaluation.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 
 const EvaluationSchema = new mongoose.Schema({
+  date: {
+    type: String,
+    required: [true, 'A date is required'],
+    trim: true,
+    match: [
+      /(\d{4})[-.\/](\d{2})[-.\/](\d{2})/,
+      'Use a valid date'
+    ]
+  },
+  time: {
+    type: String,
+    required: [true, 'A time is required'],
+    trim: true,
+    match: [
+      /^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/,
+      'Use a valid time'
+    ]
+  },
   evaluation: {
     type: String,
     required: [true, 'An evaluation is required'],
