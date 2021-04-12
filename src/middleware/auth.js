af8c788636ee227b0d9e7fcb4bcee1f5f9dd3ff1
Eduardo Marino Riscado <eduardomarino@gmail.com>
2021-04-12 12:49:10 -0700

    fix: user model import

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/userModel');
 const ErrorResponse = require('../utils/errorResponse');
 
 // Protect routes
